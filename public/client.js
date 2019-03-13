15fa6982bbf3db93bf6e4a8c039ee822ad2b0a7d
Nicholas Valenzuela <nick22793@hotmail.com>
2019-03-13 15:06:36 -0700

    added general information communication between index and client

--- public/client.js
+++ public/client.js
@@ -133,6 +133,117 @@ $('#register').on('click', function (event) {
 
 });
 
+$('#general-submit').on('click', function (event) {
+    event.preventDefault();
+    const firstName = $('.firstName').val();
+    const lastName = $('.lastName').val();
+    const diagnosis = $('.diagnosis').val();
+    const gestationalAge = $('.gestationalAge').val();
+    const correctedGestationalAgeDay = $('.correctedGestationalAgeDay').val();
+    const correctedGestationalAgeWeek = $('.correctedGestationalAgeWeek').val();
+    const dayOfLife = $('.dayOfLife').val();
+    const dayOfBirth = $('.dayOfBirth').val();
+    const timeOfBirth = $('.timeOfBirth').val();
+    const birthWeight = $('.birthWeight').val();
+    const currentWeight = $('.currentWeight').val();
+    const kaiserScore = $('.kaiserScore').val();
+    const bloodType = $('.bloodType').val();
+    const coombs = $('.coombs').val();
+    const acuity = $('.acuity').val();
+    const bedNumber = $('.bedNumber').val();
+    const dateOfAdmission = $('.dateOfAdmission').val();
+    const timeOfAdmission = $('.timeOfAdmission').val();
+    const dateOfDischarge = $('.dateOfDischarge').val();
+    const timeOfDischarge = $('.timeOfDischarge').val();
+    if (firstName == '') {
+        alert('Please enter first name.');
+    } else if (lastName == "") {
+        alert('Please enter last name');
+    } else if (diagnosis == "") {
+        alert('Please enter diagnosis');
+    } else if (gestationalAge == "") {
+        alert('Please enter gestational age');
+    } else if (correctedGestationalAgeDay == "") {
+        alert('Please enter gestational age day');
+    } else if (correctedGestationalAgeWeek == "") {
+        alert('Please enter gestational age week');
+    } else if (dayOfLife == "") {
+        alert('Please enter day of life');
+    } else if (dayOfBirth == "") {
+        alert('Please enter day of birth');
+    } else if (timeOfBirth == "") {
+        alert('Please enter time of birth');
+    } else if (birthWeight == "") {
+        alert('Please enter birth weight');
+    } else if (currentWeight == "") {
+        alert('Please enter current weight');
+    } else if (kaiserScore == "") {
+        alert('Please enter kaiser score');
+    } else if (bloodType == "") {
+        alert('Please enter bloodtype');
+    } else if (coombs == "") {
+        alert('Please enter COOMBS score');
+    } else if (acuity == "") {
+        alert('Please enter acuity score');
+    } else if (bedNumber == "") {
+        alert('Please enter bed number');
+    } else if (dateOfAdmission == "") {
+        alert('Please enter date of admission');
+    } else if (timeOfAdmission == "") {
+        alert('Please enter time of admission');
+    } else if (dateOfDischarge == "") {
+        alert('Please enter date of discharge');
+    } else if (timeOfDischarge == "") {
+        alert('Please enter time of discharge');
+    } else {
+        const generalInformationObject = {
+            firstName,
+            lastName,
+            diagnosis,
+            gestationalAge,
+            correctedGestationalAgeDay,
+            correctedGestationalAgeWeek,
+            dayOfLife,
+            dayOfBirth,
+            timeOfBirth,
+            birthWeight,
+            currentWeight,
+            kaiserScore,
+            bloodType,
+            coombs,
+            acuity,
+            bedNumber,
+            dateOfAdmission,
+            timeOfAdmission,
+            dateOfDischarge,
+            timeOfDischarge
+        };
+        console.log(generalInformationObject);
+        $.ajax({
+                type: 'POST',
+                url: '/users/create',
+                dataType: 'json',
+                data: JSON.stringify(generalInformationObject),
+                contentType: 'application/json'
+            })
+            .done(function (result) {
+                $('.js-signin-success').html('Thanks for signing up! Please sign in.');
+                $('.js-signin-success').addClass('change-status-success');
+                //            showLogInScreen();
+                //                $('#sign-up-form').addClass('hidden');
+                //                $('#landing-page-info').addClass('hidden');
+                //                $('#full-form').removeClass('hidden');
+                //                $('body').css('background', 'white');
+            })
+            .fail(function (jqXHR, error, errorThrown) {
+                console.log(jqXHR);
+                console.log(error);
+                console.log(errorThrown);
+            });
+    };
+
+});
+
 $('#sign-up-link').on('click', function (event) {
     event.preventDefault();
     $('#login-form').addClass('hidden');
