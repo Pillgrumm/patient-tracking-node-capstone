fe1949adf34f591d9a769a07358231b02dfe5201
Nicholas Valenzuela <nick22793@hotmail.com>
2019-04-08 11:41:07 -0700

    testing to see if config will allow heroku deployment

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
 exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://demo:123456a@ds151805.mlab.com:51805/patient-tracking';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
-'mongodb://demo:123456a@ds151805.mlab.com:51805/patient-tracking';
-exports.PORT = process.env.PORT || 8080;
+    'mongodb://demo:123456a@ds151805.mlab.com:51805/patient-tracking';
+exports.PORT = process.env.PORT;
