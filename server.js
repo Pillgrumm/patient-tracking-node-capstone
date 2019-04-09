21ac56d6dc40702fd2e4c6ec0a316583d06ce61e
Nicholas Valenzuela <nick22793@hotmail.com>
2019-04-08 17:13:58 -0700

    removed app.get in server.js

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.use(express.static('public'));
 mongoose.Promise = global.Promise;
 
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/public/index.html');
-});
+//app.get('/', function (req, res) {
+//    res.sendFile(__dirname + '/public/index.html');
+//});
 
 
 
